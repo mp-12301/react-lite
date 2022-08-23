1de5a9d6ababa2aa249226138dd1515395a362ca
mp <mp.262gr@slmail.me>
2022-08-23 13:11:59 +0100

    wip

--- src/index.js
+++ src/index.js
@@ -11,7 +11,24 @@ function createElement(type, props, ...children) {
   }
 }
 
-function render(element, container) {
+function createDom(fiber) {
+  const dom =
+    fiber.type === "TEXT_ELEMENT"
+      ? document.createTextNode("")
+      : document.createElement(fiber.type)
+
+  const isProperty = key => key !== "children"
+  Object.keys(fiber.props)
+    .filter(isProperty)
+    .forEach(name => {
+      dom[name] = fiber.props[name]
+    })
+
+  return dom
+}
+
+
+function _render(element, container) {
   const dom =
     element.type === "TEXT_ELEMENT"
       ? document.createTextNode("")
@@ -30,16 +47,85 @@ function render(element, container) {
   container.appendChild(dom)
 }
 
+function commitRoot() {
+  // TODO add nodes to dom
+}
+
+function render(element, container) {
+  wipRoot = {
+    dom: container,
+    props: {
+      children: [element],
+    },
+  }
+  nextUnitOfWork = wipRoot
+}
+
 let nextUnitOfWork = null
+let wipRoot = null
 
 function workLoop(deadline) {
   let shouldYield = false
+
+  while (nextUnitOfWork && !shouldYield) {
+    nextUnitOfWork = performUnitOfWork(
+      nextUnitOfWork
+    )
+    shouldYield = deadline.timeRemaining() < 1
+  }
+
+  if (!nextUnitOfWork && wipRoot) {
+    commitRoot()
+  }
+
+  requestIdleCallback(workLoop)
 }
 
+// React doesn't use requestIdleCallback anymore.
+// Now it uses the scheduler package.
 requestIdleCallback(workLoop)
 
-function performUnitOfWork(nextUnitOfWork) {
+function performUnitOfWork(fiber) {
+  if (!fiber.dom) {
+    fiber.dom = createDom(fiber)
+  }
+
+  const elements = fiber.props.children
+  let index = 0
+  let prevSibling = null
+
+  while (index < elements.length) {
+    const element = elements[index]
+
+    const newFiber = {
+      type: element.type,
+      props: element.props,
+      parent: fiber,
+      dom: null,
+    }
+
+    if (index === 0) {
+      fiber.child = newFiber
+    } else {
+      prevSibling.sibling = newFiber
+    }
+
+    prevSibling = newFiber
+    index++
+  }
+
+  if (fiber.child) {
+    return fiber.child
+  }
+  let nextFiber = fiber
+  while (nextFiber) {
+    if (nextFiber.sibling) {
+      return nextFiber.sibling
+    }
+    nextFiber = nextFiber.parent
+  }
 }
+
 
 function createTextElement(text) {
   return {
@@ -53,7 +139,7 @@ function createTextElement(text) {
 
 const ReactLite = {
   createElement,
-  render,
+  render: _render,
 }
 
 /** @jsx ReactLite.createElement */
